89085b37d8deeaa42735da83c8f26fc8bff34779
mguevara717 <marissa.guevara717@gmail.com>
2018-11-08 11:14:11 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React, {Component} from 'react'
+
+const MovieReviews = (props) => {
+  const {reviews} = props
+
+  return (
+  
+    <ol className="review-list">
+        {reviews.map(review => <li className='review' key={review.date_updated + review.display_title}>{review.display_title}</li>)}
+    </ol>
+
+  )
+
+
+
+} //end of functional component
+
+export default MovieReviews
